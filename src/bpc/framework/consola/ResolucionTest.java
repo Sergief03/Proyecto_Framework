7576d6c445fb282d3e2738e9015565c34cb8f42e
ialva <ianaltri@gmail.com>
2025-04-29 13:10:05 +0200

    Test del programa resolución

---
+++ src/bpc/framework/consola/ResolucionTest.java
@@ -0,0 +1,29 @@
+package bpc.framework.consola;
+
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class ResolucionTest {
+    public void Test1(){
+        Dimension[] dimensiones = {
+                new Dimension(4096, 2160),
+                new Dimension(1920, 1080),
+                new Dimension(2560, 1440),
+                new Dimension(800, 600)
+        };
+
+        Resolucion[] resoluciones = {
+                Resolucion.VENTANA_4K,
+                Resolucion.VENTANA_FULLHD,
+                Resolucion.VENTANA_QHD,
+                Resolucion.VENTANA_VGA
+        };
+
+        for (int i = 0;i<dimensiones.length;i++){
+            assertEquals(dimensiones[i], resoluciones[i].getResolution());
+        }
+    }
+}
