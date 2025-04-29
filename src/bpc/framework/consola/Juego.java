158d4d77c8b7dfa91517c874500b9fb1df91d128
fjfh06 <fferher513h@g.educaand.es>
2025-04-29 12:25:17 +0200

    Hecho stub juego

--- src/bpc/framework/consola/Juego.java
+++ src/bpc/framework/consola/Juego.java
@@ -1,4 +1,28 @@
 package bpc.framework.consola;
 
+import bpc.daw.consola.Consola;
+
 public class Juego {
+    private Consola consola;
+    private Escena escena;
+    private boolean detener;
+    private Resolucion resolucion;
+    public Juego(){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public void iniciar(Escena e, Resolucion r){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public Escena getEscena(){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public void detener(){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public int getAnchuraPantalla(){
+        throw new UnsupportedOperationException("Sin programar");
+    }
+    public int getAlturaPantalla(){
+        throw new UnsupportedOperationException("Sin programar");
+    }
 }
